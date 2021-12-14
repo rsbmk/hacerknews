b0bb320e4dd4536d0f52917bb64e2d97827a900e
Roberto Bocio Melo <elpicapica01@gmail.com>
2021-12-14 12:43:00 -0500

    feat(Root): default 404 page

--- src/pages/default404.tsx
+++ src/pages/default404.tsx
@@ -1,12 +1,17 @@
+import { Title } from 'react-head'
 import { Link } from 'wouter'
 
 export function Default404 () {
   return (
-    <p className='default404'>
-      Ops, the page you are looking for cannot be found. <br/>
-      <strong>
-        You can go back to <Link href="/all"> all the news.</Link>
-      </strong>
-    </p>
+    <>
+      <Title>404 Not Found| Hacker News</Title>
+
+      <p className="default404">
+        Ops, the page you are looking for cannot be found. <br />
+        <strong>
+          You can go back to <Link href="/all"> all the news.</Link>
+        </strong>
+      </p>
+    </>
   )
 }
