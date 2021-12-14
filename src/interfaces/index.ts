02ef45db46a22fd7e91a64eb4e043fb945382517
Roberto Bocio Melo <elpicapica01@gmail.com>
2021-12-14 12:45:09 -0500

    refactor(Root): chenge singleNews interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,20 +1,10 @@
 export interface singleNews {
-  author: string;
-  commen_text: string;
-  created_at: string;
-  created_at_i: number;
-  num_comments: number | null;
   objectID: string;
-  parent_id: number;
-  points: number | null;
-  story_id: number;
-  story_text: string | null;
+  author: string;
+  created_at: string;
   story_title: string;
   story_url: string;
-  title: string | null;
-  url: string | null;
 }
-
 export interface NewsData {
   exhaustiveNbHits: boolean;
   exhaustiveTypo: boolean;
