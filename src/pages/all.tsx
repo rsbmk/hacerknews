c5b3bbba01a34007e630153450dc6dde04fd8776
Roberto Bocio Melo <elpicapica01@gmail.com>
2021-12-14 12:47:43 -0500

    Feat(Root): add header of the page

--- src/pages/all.tsx
+++ src/pages/all.tsx
@@ -1,19 +1,20 @@
-
 import { useState } from 'react'
 import { NewsList } from '../components/newsList'
 import { SelectTopic } from '../components/searchNews'
-import { UsetNews } from '../hooks/news'
+import { UseNews } from '../hooks/news'
+import { AllNewsHeader } from '../components/headers/allnews'
 
 export function AllNews () {
-  // save topic in the localStorage
-  const [topic, setTopic] = useState(window.localStorage.getItem('topic') || 'reactjs')
-  const { arrayNewsData, isError, loading } = UsetNews({ topic })
+  // read topic in the localStorage
+  const [topic, setTopic] = useState(() => window.localStorage.getItem('topic') || 'reactjs')
+  const { arrayNewsData, isError, loading } = UseNews({ topic })
 
   if (isError) return <h1>There was an error getting the news results.</h1>
   if (loading) return <h1>Loading...</h1>
 
   return (
     <>
+      <AllNewsHeader/>
       <SelectTopic setTopic={setTopic} topic={topic} />
       <NewsList arrayNews={arrayNewsData} />
     </>
