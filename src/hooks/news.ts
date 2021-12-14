2d69562d8053fc4f1143c198eaf2aa8814320a9e
Roberto Bocio Melo <elpicapica01@gmail.com>
2021-12-14 12:44:24 -0500

    Fix(Root): typo in the name component

--- src/hooks/news.ts
+++ src/hooks/news.ts
@@ -2,7 +2,7 @@ import useSWR from 'swr'
 import { getURL, fetcher } from './utils/getNewsData'
 import { NewsData } from '../interfaces'
 
-export function UsetNews ({ topic }: { topic: string }) {
+export function UseNews ({ topic }: { topic: string }) {
   const URI_API = getURL({ topic })
   const { data, error } = useSWR<NewsData>(URI_API, fetcher)
 
