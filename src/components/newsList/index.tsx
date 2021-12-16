c77f79105474bef030ff7a9ee72286676b985ce6
Roberto Bocio Melo <elpicapica01@gmail.com>
2021-12-15 20:19:20 -0500

    fix(Root): don't show broken news

--- src/components/newsList/index.tsx
+++ src/components/newsList/index.tsx
@@ -3,12 +3,11 @@ import { NewsCard } from '../newsCard'
 
 export function NewsList ({ arrayNews }: { arrayNews: singleNews[] | undefined }) {
   return (
-    <>
-      <main className="newsList">
-        {arrayNews?.map((news) => (
-          <NewsCard key={news.objectID} {...news} />
-        ))}
-      </main>
-    </>
+    <main className="newsList">
+      {arrayNews?.map((news) => {
+        if (!news.story_url) return null
+        return <NewsCard key={news.objectID} {...news} />
+      })}
+    </main>
   )
 }
